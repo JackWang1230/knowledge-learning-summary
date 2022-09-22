ef9b229f4ea358b6de2e53c6e31ed366ff26ef98
wangrui <jackwang9312@163.com>
2022-09-22 22:30:10 +0800

    add udtf demo

--- hive-study/src/main/java/cn/wr/udtf/ExplodeJsonDataDemoUDTF.java
+++ hive-study/src/main/java/cn/wr/udtf/ExplodeJsonDataDemoUDTF.java
@@ -3,10 +3,16 @@ package cn.wr.udtf;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -17,10 +23,34 @@ import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
  */
 
 public class ExplodeJsonDataDemoUDTF extends GenericUDTF {
+
+
     @Override
-    public StructObjectInspector initialize(StructObjectInspector argOIs) throws UDFArgumentException {
-        return super.initialize(argOIs);
+    public StructObjectInspector initialize(ObjectInspector[] args) throws UDFArgumentException {
+
+
+        if (args.length != 1) {
+            throw new UDFArgumentLengthException("ExplodeMap takes only one argument");
+        }
+        if (args[0].getCategory() != ObjectInspector.Category.PRIMITIVE) {
+            throw new UDFArgumentException("ExplodeMap takes string as a parameter");
+        }
+
+        ArrayList<String> fieldNames = new ArrayList<String>();
+        ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
+        fieldNames.add("col1");
+        fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+        fieldNames.add("col2");
+        fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+
+        return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames,fieldOIs);
     }
+
+/*    @Override
+    public StructObjectInspector initialize(StructObjectInspector argOIs) throws UDFArgumentException {
+
+        return super.initialize(argOIs);
+    }*/
 
     @Override
     public void process(Object[] args) throws HiveException {
