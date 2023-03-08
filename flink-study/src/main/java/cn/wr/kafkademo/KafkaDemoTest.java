fe8ad57863af0bcbbb55b99aa5898dff8a774eaa
jackwang1230 <jackwang9312@163.com>
2023-03-08 15:37:47 +0800

    1.测试 copilot 插件提示功能

--- flink-study/src/main/java/cn/wr/kafkademo/KafkaDemoTest.java
+++ flink-study/src/main/java/cn/wr/kafkademo/KafkaDemoTest.java
@@ -92,7 +92,23 @@ public class KafkaDemoTest {
         data.sinkTo(build);
 
 
+    }
 
-
+    @Test
+    public void bubbleSort(){
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
