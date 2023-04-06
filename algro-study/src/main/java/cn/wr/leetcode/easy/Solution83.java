e41252e9838e9b2b27fec51492038beb880f7772
jackwang1230 <jackwang9312@163.com>
2023-04-06 15:52:18 +0800

    leetcode solution83 easy

---
+++ algro-study/src/main/java/cn/wr/leetcode/easy/Solution83.java
@@ -0,0 +1,60 @@
+package cn.wr.leetcode.easy;
+
+
+//给定一个已排序的链表的头
+// head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,1,2]
+//输出：[1,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点数目在范围 [0, 300] 内
+// -100 <= Node.val <= 100
+// 题目数据保证链表已经按升序 排列
+//
+//
+// Related Topics 链表 👍 971 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * @author : WangRui
+ * @date : 2023/4/6
+ */
+
+public class Solution83 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+}
