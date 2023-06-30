13bbd0ced0e5fb941dc6420bcbdb6e89b52932c7
jackwang1230 <jackwang9312@163.com>
2023-06-30 10:29:44 +0800

    add comments

--- java-study/src/main/java/cn/wr/abstracts/Son.java
+++ java-study/src/main/java/cn/wr/abstracts/Son.java
@@ -15,8 +15,20 @@ public abstract class Son<T> implements Parent<T> {
         this.aaa = Long.MIN_VALUE + ss.toMilliseconds();
     }
 
+    /***
+     * 子类继承的父类为抽象类 必须重写其抽象方法
+     * @param element
+     * @return
+     */
     public abstract long print(T element);
 
+
+    /**
+     * final 修饰的方法 不能被重写
+     * @param dd
+     * @param aa
+     * @return
+     */
     @Override
     public final long print(T dd, long aa) {
         long print = print(dd);
